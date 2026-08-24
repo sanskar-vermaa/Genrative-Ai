fc8e48eac3a3ec483cb27419e29d15a5606a714b
sanskar-vermaa <sanskarv120503@gmail.com>
2026-08-24 12:14:04 +0530

    Add app shell Layout with sidebar styling

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar.jsx';
+
+export default function Layout() {
+  return (
+    <div className="app-shell">
+      <Sidebar />
+      <main className="content">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
